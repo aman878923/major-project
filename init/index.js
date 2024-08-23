55c8f4d75749388e6bc1bbb4e9043476e5a0e1af
aman878923 <aman.agarwal7385@gmail.com>
2024-08-23 19:25:52 +0530

    added

--- init/index.js
+++ init/index.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 const initData = require("./data");
 const Listing = require("../models/listing.js");
 
-main()
-  .then((res) => console.log("mongoose connected"))
-  .catch((err) => console.log(err));
-  const URL = process.env.ATLASPASSWORD;
+// main()
+//   .then((res) => console.log("mongoose connected"))
+//   .catch((err) => console.log(err));
+//   const URL = process.env.ATLASPASSWORD;
 
 async function main() {
-  await mongoose.connect(/* "mongodb://127.0.0.1:27017/wanderlust" */ URL);
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust" );
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
